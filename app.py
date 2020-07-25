b83c2c8b3a24300f373ff888359494bcc863d88e
kurianbenoy <kurian.bkk@gmail.com>
2020-07-25 20:06:03 +0530

    fix and add cartoonize property of app

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def resize_crop(image):
     return image
 
 
-def cartoonize(load_folder, save_folder, model_path):
+def cartoonize(img_name,load_folder, save_folder, model_path):
     input_photo = tf.placeholder(tf.float32, [1, None, None, 3])
     network_out = network.unet_generator(input_photo)
     final_out = guided_filter.guided_filter(input_photo, network_out, r=1, eps=5e-3)
@@ -37,21 +37,16 @@ def cartoonize(load_folder, save_folder, model_path):
     sess.run(tf.global_variables_initializer())
     saver.restore(sess, tf.train.latest_checkpoint(model_path))
     name_list = os.listdir(load_folder)
-    for name in tqdm(name_list):
-        try:
-            load_path = os.path.join(load_folder, name)
-            save_path = os.path.join(save_folder, name)
-            image = cv2.imread(load_path)
-            image = resize_crop(image)
-            batch_image = image.astype(np.float32)/127.5 - 1
-            batch_image = np.expand_dims(batch_image, axis=0)
-            output = sess.run(final_out, feed_dict={input_photo: batch_image})
-            output = (np.squeeze(output)+1)*127.5
-            output = np.clip(output, 0, 255).astype(np.uint8)
-            cv2.imwrite(save_path, output)
-        except:
-            print('cartoonize {} failed'.format(load_path))
-
+    load_path = os.path.join(load_folder, img_name)
+    save_path = os.path.join(save_folder, img_name)
+    image = cv2.imread(load_path)
+    image = resize_crop(image)
+    batch_image = image.astype(np.float32)/127.5 - 1
+    batch_image = np.expand_dims(batch_image, axis=0)
+    output = sess.run(final_out, feed_dict={input_photo: batch_image})
+    output = (np.squeeze(output)+1)*127.5
+    output = np.clip(output, 0, 255).astype(np.uint8)
+    cv2.imwrite(save_path, output)
 
 app = Flask(__name__)
 UPLOAD_FOLDER = "static/upload/"
@@ -71,9 +66,10 @@ def home():
 if __name__ == "__main__":
     model_path = 'saved_models'
     load_folder = 'static/upload/'
-    #img_name = '/home/kurian/Projects/White-box-Cartoonization/static/upload/a.jpg'
+    img_name = 'kurian_mecpodcast.jpg'
     save_folder = 'static/cartoonized_images/'
- #   app.run(port=1200, debug=True)
     if not os.path.exists(save_folder):
         os.mkdir(save_folder)
-    cartoonize(load_folder, save_folder, model_path)
+    cartoonize(img_name,load_folder, save_folder, model_path)
+
+    #   app.run(port=1200, debug=True)
