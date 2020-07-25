694f04b25078326c312e797d6ffd5b4fe02d0b1f
kurianbenoy <kurian.bkk@gmail.com>
2020-07-25 13:41:48 +0530

    add upload folder

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
+import os
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
-@app.route("/")
+UPLOAD_FOLDER = "static/upload/"
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template('toon_it.html')
+    if request.method == "POST":
+        image_file = request.files["image"]
+        if image_file:
+            image_location = os.path.join(UPLOAD_FOLDER, image_file.filename)
+            image_file.save(image_location)
+            return render_template('toon_it.html', predictions=1)
+    return render_template('toon_it.html', predictions=0)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=1200,debug=True)
