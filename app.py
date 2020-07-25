9b1211c53c7a2be2d14bcc606646fc3d731e3286
kurianbenoy <kurian.bkk@gmail.com>
2020-07-25 19:52:35 +0530

    add code in flask app todo cartoonize inside static folder

--- app.py
+++ app.py
@@ -21,36 +21,36 @@ def resize_crop(image):
     return image
 
 
-def cartoonize(img_name, save_folder, model_path):
+def cartoonize(load_folder, save_folder, model_path):
     input_photo = tf.placeholder(tf.float32, [1, None, None, 3])
     network_out = network.unet_generator(input_photo)
     final_out = guided_filter.guided_filter(input_photo, network_out, r=1, eps=5e-3)
 
     all_vars = tf.trainable_variables()
-    gene_vars = [var for var in all_vars if "generator" in var.name]
+    gene_vars = [var for var in all_vars if 'generator' in var.name]
     saver = tf.train.Saver(var_list=gene_vars)
-
+    
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
     sess = tf.Session(config=config)
 
     sess.run(tf.global_variables_initializer())
     saver.restore(sess, tf.train.latest_checkpoint(model_path))
-    # name_list = os.listdir(load_folder)
-    # for name in tqdm(name_list):
-    #     try:
-    load_path = os.path.join(load_folder, img_name)
-    save_path = os.path.join(save_folder, img_name)
-    image = cv2.imread(load_path)
-    image = resize_crop(image)
-    batch_image = image.astype(np.float32) / 127.5 - 1
-    batch_image = np.expand_dims(batch_image, axis=0)
-    output = sess.run(final_out, feed_dict={input_photo: batch_image})
-    output = (np.squeeze(output) + 1) * 127.5
-    output = np.clip(output, 0, 255).astype(np.uint8)
-    cv2.imwrite(save_path, output)
-        # except:
-        #     print("cartoonize {} failed".format(load_path))
+    name_list = os.listdir(load_folder)
+    for name in tqdm(name_list):
+        try:
+            load_path = os.path.join(load_folder, name)
+            save_path = os.path.join(save_folder, name)
+            image = cv2.imread(load_path)
+            image = resize_crop(image)
+            batch_image = image.astype(np.float32)/127.5 - 1
+            batch_image = np.expand_dims(batch_image, axis=0)
+            output = sess.run(final_out, feed_dict={input_photo: batch_image})
+            output = (np.squeeze(output)+1)*127.5
+            output = np.clip(output, 0, 255).astype(np.uint8)
+            cv2.imwrite(save_path, output)
+        except:
+            print('cartoonize {} failed'.format(load_path))
 
 
 app = Flask(__name__)
@@ -69,4 +69,11 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(port=1200, debug=True)
+    model_path = 'saved_models'
+    load_folder = 'static/upload/'
+    #img_name = '/home/kurian/Projects/White-box-Cartoonization/static/upload/a.jpg'
+    save_folder = 'static/cartoonized_images/'
+ #   app.run(port=1200, debug=True)
+    if not os.path.exists(save_folder):
+        os.mkdir(save_folder)
+    cartoonize(load_folder, save_folder, model_path)
